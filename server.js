c1b44e0e2b92c0283d31351f23a72a324ba2ba48
Yulber Espinoza <yulbertime@gmail.com>
2024-10-08 15:51:51 -0500

    commit final

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes/route");
+
+const app = express();
+app.use(cors());
+
+// definimos PORT
+const PORT = process.env.PORT || 3001;
+
+// definimos route
+app.use(routes);
+app.use("*", (request, response) =>
+  response.status(404).send("Page not found :C")
+);
+
+// puerto donde escucha el server
+app.listen(PORT, () => console.log(`El servidor esta corriendo en :${PORT}.`));
